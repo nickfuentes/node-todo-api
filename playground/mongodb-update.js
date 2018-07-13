87ada25fbd130acc023c36a11aaba88a32f00fc1
Nickolas Fuentes <nickolasfuentes@Mark-1.attlocal.net>
2018-07-12 20:49:53 -0500

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5b47f7f2689e499cc9cab65a')
+    // },  {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5b462a21bfed6c7afb2a09d4')
+    },  {
+        $set: {
+            name: 'Nickolas'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // db.close();
+});
